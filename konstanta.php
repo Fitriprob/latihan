59c3aca1002a514cd5b249083d140f6b195205df
Fitri <80827445+Fitriprob@users.noreply.github.com>
2021-10-27 19:21:31 +0700

    Add files via upload
    Nama : Fitri
    Nim    : 20122316

---
+++ konstanta.php
@@ -0,0 +1,8 @@
+<?php
+define ("Hobi", "Bersepeda");
+echo "Assalamualaikum Hobi Saya :";
+echo Hobi;
+$jl=153;
+echo" Dan Alamat Saya Di Jalan Amir Hamzah No.153, RT 004 RW 004, Kel. Kedungasem, Kec. Wonoasih";
+echo $jl;
+?>
